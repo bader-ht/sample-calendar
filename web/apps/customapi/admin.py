6f0993a657bf18fa0e9d2c43d75bc95f6524b685
Bader Alsayegh <>
2022-02-03 00:06:07 +0300

    added reservation model to admin for convenience

--- web/apps/customapi/admin.py
+++ web/apps/customapi/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from apps.customapi.models import Calendar
+from apps.customapi.models import Calendar, Reservation
 
 class CalendarAdmin(admin.ModelAdmin):
     list_display = ['user', 'start_date', 'end_date']    
 
 admin.site.register(Calendar, CalendarAdmin)
+admin.site.register(Reservation, CalendarAdmin) 
