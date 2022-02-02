59d26cc32bd7ca3bb6b32b269b479d7ee603e509
Bader Alsayegh <>
2022-02-02 22:27:19 +0300

    unified file naming - serializers

--- web/apps/customapi/views/basic.py
+++ web/apps/customapi/views/basic.py
@@ -2,7 +2,7 @@ from rest_framework import response, status, views, exceptions
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework import generics
-from apps.customapi.serializers.custom import *
+from apps.customapi.serializers.basic import *
 from apps.customapi.permissions import *
 from apps.customapi.models import *
 
