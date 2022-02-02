ea4c84ad2e5acfbb739f8ee202a1ccfc6fb50e81
Bader Alsayegh <>
2022-02-02 19:41:19 +0300

    added validation to prevent overlapping schedules for logged in users

--- web/apps/customapi/views/basic.py
+++ web/apps/customapi/views/basic.py
@@ -5,15 +5,37 @@ from rest_framework import generics
 from apps.customapi.serializers.custom import *
 
 class CalendarCreateAPIView(generics.CreateAPIView):
+    # Permission class to ensure user is logged in to access this view
+    permission_classes = [IsAuthenticated] 
     serializer_class = CalendarSerializer
 
-class CalendarRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    def create(self, request, *args, **kwargs):
+        data = request.data
 
+        if self.request.user.is_authenticated:
+            data["user"] = self.request.user.id
+            # data["email"] = None
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+class CalendarRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = CalendarSerializer
 
 
 class CalendarDeleteAPIView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = CalendarSerializer
 
 class CalendarDeleteAPIView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = CalendarSerializer
+
+    # def get_queryset(self):
+    #     return 
